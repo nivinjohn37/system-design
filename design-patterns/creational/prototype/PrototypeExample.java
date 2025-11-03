67c16573c7b9072142c5aeca955df9fe7676588c
Nivin Shaji John <getnivin@gmail.com>
2025-11-03 22:49:00 +0530

    prototype

---
+++ design-patterns/creational/prototype/PrototypeExample.java
@@ -0,0 +1,39 @@
+package creational.prototype;
+
+interface Prototype extends Cloneable {
+    Prototype clone();
+}
+
+class Document implements Prototype {
+    private String title;
+    public Document(String title) {
+        this.title = title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Prototype clone() {
+        try {
+            return (Document) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Document [title=" + title + "]";
+    }
+}
+public class PrototypeExample {
+    public static void main(String[] args) {
+        Document document = new Document("Document");
+        Document clone = (Document) document.clone();
+        clone.setTitle("Document Copy");
+        System.out.println(document);
+        System.out.println(clone);
+
+    }
+}
